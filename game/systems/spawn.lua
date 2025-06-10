cea93c7650756b216fd8b8164e964056c1ee5acc
Tobias Gärdebro <tubbeg@gmail.com>
2025-06-10 22:54:38 +0200

    Working on adding different shapes.

--- game/systems/spawn.lua
+++ game/systems/spawn.lua
@@ -19,14 +19,28 @@ function GetNumberOfMovingCubes(ents)
     return nr
 end
 
+function AddTriangle(origin)
+    local form = {}
+    return form
+end
+
+function AddRandomCubeForm(origin)
+    --math.randomseed(os.time())
+    --print(math.random(), "random")
+    local form = AddTriangle(origin)
+    return form
+end
+
 
 function spawnCubes:update(dt)
     local n = GetNumberOfMovingCubes(self.entities)
     if n == 0 then
-        local entity = {
-            cube = {x = 3, y = 0}
-        }
-        tiny.addEntity(self.world,entity)
+        local ent3 = {cube = {x = 4, y = 0}}
+        local ent2 = {cube = {x = 3, y = 0}}
+        local ent1 =  {cube = {x = 2, y = 0}}
+        tiny.addEntity(self.world, ent1)
+        tiny.addEntity(self.world, ent2)
+        tiny.addEntity(self.world, ent3)   
     end
 end
 
